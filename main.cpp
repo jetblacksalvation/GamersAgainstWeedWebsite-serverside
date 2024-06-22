2638c99549067d94d8a6b1246b2ada8c13f69f3f
ed <ed@ProgrammingEnvW>
2024-06-22 21:41:52 +0000

    the code

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿// GamersAgainstWeedWebsiteCPlusPlus.cpp : Defines the entry point for the application.
+//
+
+#include "Server.h"
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
